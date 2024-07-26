b8e075dc369241c89f6b93f669ca39ca40a4992f
umaimahashmi <157885208+umaimahashmi@users.noreply.github.com>
2024-07-26 14:18:43 +0500

    adding files

---
+++ script.js
@@ -0,0 +1,111 @@
+const questions = [
+  {
+    question: "which is largest animal?",
+    answers: [
+      { text: "shark", correct: false },
+      { text: "blue whale", correct: true },
+      { text: "elephant", correct: false },
+      { text: "giraffe", correct: false },
+    ],
+  },
+  {
+    question: "which is smallest continent?",
+    answers: [
+      { text: "asia", correct: false },
+      { text: "australia", correct: true },
+      { text: "arctic", correct: false },
+      { text: "africa", correct: false },
+    ],
+  },
+  {
+    question: "which is capital of china?",
+    answers: [
+      { text: "isl", correct: false },
+      { text: "korea", correct: false },
+      { text: "NY", correct: false },
+      { text: "Beijin", correct: true },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct == "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct == "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You Scored ${score} out of ${questions.length}!`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextButton();
+  } else {
+    startQuiz();
+  }
+});
+
+startQuiz();
